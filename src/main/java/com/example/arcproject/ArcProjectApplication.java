4768de36aef6b40c8910b046f9afbfb600acaeca
August Skipper <ausk0001@stud.ek.dk>
2026-02-25 13:24:00 +0100

    Initial commit

---
+++ src/main/java/com/example/arcproject/ArcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.arcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arcproject/ArcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
