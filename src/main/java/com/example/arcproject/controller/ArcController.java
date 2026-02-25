9004b70c54700fd65c3db8c792a3fc3782837d9d
August Skipper <ausk0001@stud.ek.dk>
2026-02-25 15:17:53 +0100

    Created the controller class, added showAllArcs method and created our first HTML file.

---
+++ src/main/java/com/example/arcproject/controller/ArcController.java
@@ -0,0 +1,27 @@
+package com.example.arcproject.controller;
+
+import com.example.arcproject.service.ArcService;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/arcs")
+public class ArcController {
+
+private final ArcService service;
+
+public ArcController(ArcService service) {
+    this.service = service;
+}
+@GetMapping
+    public String showAllArcs(Model model) {
+    model.addAttribute("arcs", service.getArcList());
+    return "listOfArcs";
+}
+
+
+
+}
