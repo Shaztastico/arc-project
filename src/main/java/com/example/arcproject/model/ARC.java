f242368162ec4dff2b93234f56c8c8c2265a2656
August Skipper <ausk0001@stud.ek.dk>
2026-02-25 14:34:04 +0100

    Created ARC and ArcRepository classes and added the relevant fields and methods to them both.

---
+++ src/main/java/com/example/arcproject/model/ARC.java
@@ -0,0 +1,63 @@
+package com.example.arcproject.model;
+
+import java.util.List;
+
+public class ARC {
+    private List<String> workbenches;
+    private String name;
+    private boolean isLarge;
+    private boolean canFly;
+    private boolean isStationary;
+
+    public ARC(String name, boolean isLarge, boolean canFly, boolean isStationary, List<String> workbenches) {
+        this.name = name;
+        this.isLarge = isLarge;
+        this.canFly = canFly;
+        this.isStationary = isStationary;
+        this.workbenches = workbenches;
+    }
+
+    // Empty constructor to be used for our /add method in the controller.
+    public ARC() {
+    }
+
+    public List<String> getWorkbenches() {
+        return workbenches;
+    }
+
+    public void setWorkbenches(List<String> workbenches) {
+        this.workbenches = workbenches;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLarge() {
+        return isLarge;
+    }
+
+    public void setLarge(boolean large) {
+        isLarge = large;
+    }
+
+    public boolean isCanFly() {
+        return canFly;
+    }
+
+    public void setCanFly(boolean canFly) {
+        this.canFly = canFly;
+    }
+
+    public boolean isStationary() {
+        return isStationary;
+    }
+
+    public void setStationary(boolean stationary) {
+        isStationary = stationary;
+    }
+}
---
+++ src/main/java/com/example/arcproject/repository/ArcRepository.java
@@ -0,0 +1,59 @@
+package com.example.arcproject.repository;
+
+import com.example.arcproject.model.ARC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArcRepository {
+    private List<ARC> arcList = new ArrayList<>();
+    private final List<String> workbenches = new ArrayList<>(List.of("Workbench", "Gunsmith", "Gear Bench",
+            "Utility Station", "Explosives Station",
+            "Medical Lab", "Refiner"));
+
+    public ArcRepository() {
+        populateArcList();
+    }
+
+    public ARC findByName(String searchName) {
+        for (ARC arc : arcList) {
+            if (arc.getName().equalsIgnoreCase(searchName)) {
+                return arc;
+            }
+        }
+        return null;
+    }
+
+    private ARC add(ARC arc) {
+        if (arc.getName() != null) {
+            arcList.add(arc);
+        }
+        return arc;
+    }
+
+
+    private ARC deleteFromList(String name) {
+        ARC foundArc = findByName(name);
+        if (foundArc != null) {
+            arcList.remove(foundArc);
+            return foundArc;
+        }
+        return null;
+    }
+
+    public List<ARC> getArcList() {
+        return arcList;
+    }
+
+    public List<String> getWorkbenches() {
+        return workbenches;
+    }
+
+    private void populateArcList() {
+        add(new ARC("Rocketeer", true, true, false, List.of("Explosives Station")));
+        add(new ARC("Tick", false, false, false, List.of("Medical Lab")));
+        add(new ARC("Snitch", false, true, false, List.of("Utility Station")));
+    }
+
+
+}
