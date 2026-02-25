24535645ef05d5e24c16f3ff0dd95619514d6981
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-25 16:13:00 +0100

    added GetMapping in controller for arcFoundByName, and added the html class to match

--- src/main/java/com/example/arcproject/controller/ArcController.java
+++ src/main/java/com/example/arcproject/controller/ArcController.java
@@ -5,23 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/arcs")
 public class ArcController {
 
-private final ArcService service;
+    private final ArcService service;
 
-public ArcController(ArcService service) {
-    this.service = service;
-}
-@GetMapping
+    public ArcController(ArcService service) {
+        this.service = service;
+    }
+
+    @GetMapping
     public String showAllArcs(Model model) {
-    model.addAttribute("arcs", service.getArcList());
-    return "listOfArcs";
-}
+        model.addAttribute("arcs", service.getArcList());
+        return "listOfArcs";
+    }
 
-
+    @GetMapping("/{name}")
+    public String findArcByName(@PathVariable String name, Model model){
+        model.addAttribute("arc", service.findByName(name));
+        return "arcFoundByName";
+    }
 
 }
