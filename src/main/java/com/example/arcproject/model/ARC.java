452e65c8b0811cea6c1c47dc0217be0e573e6aa9
August Skipper <ausk0001@stud.ek.dk>
2026-02-27 15:16:44 +0100

    Created Add method basic framework, and made an HTML file with a name and description text box

--- src/main/java/com/example/arcproject/model/ARC.java
+++ src/main/java/com/example/arcproject/model/ARC.java
@@ -11,20 +11,8 @@ public class ARC {
     private boolean hasLegs;
     private String description;
 
-    public boolean isHasLegs() {
-        return hasLegs;
-    }
-
-    public void setHasLegs(boolean hasLegs) {
-        this.hasLegs = hasLegs;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    // Empty constructor to be used for our /add method in the controller.
+    public ARC() {
     }
 
     public ARC(String name, boolean isLarge, boolean canFly, boolean isStationary, boolean hasLegs,
@@ -38,9 +26,6 @@ public class ARC {
         this.workbenches = workbenches;
     }
 
-    // Empty constructor to be used for our /add method in the controller.
-    public ARC() {
-    }
 
     public List<String> getWorkbenches() {
         return workbenches;
@@ -81,4 +66,22 @@ public class ARC {
     public void setStationary(boolean stationary) {
         isStationary = stationary;
     }
+
+
+    public boolean isHasLegs() {
+        return hasLegs;
+    }
+
+    public void setHasLegs(boolean hasLegs) {
+        this.hasLegs = hasLegs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
